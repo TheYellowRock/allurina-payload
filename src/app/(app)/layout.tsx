eb39b5d9d422e2a7eb62ac376d4a2a9b9b3dbe55
TheYellowRock <122740115+TheYellowRock@users.noreply.github.com>
2026-06-19 21:33:18 +0200

    CAPI Fix

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -50,17 +50,25 @@ export default function AppLayout({
         {GTM_ID ? <GoogleTagManager gtmId={GTM_ID} /> : null}
         {PIXEL_ID ? (
           <>
-            <Script id="fb-pixel" strategy="afterInteractive">{`
-              !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){
-              n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window,document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init','${PIXEL_ID}');fbq('track','PageView');
-            `}</Script>
-            <Suspense>
+            <Script
+              id="fb-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  console.log('[Meta Pixel] Pixel ID:', '${PIXEL_ID}');
+                  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){
+                  n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window,document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init','${PIXEL_ID}');
+                  fbq('track','PageView');
+                `,
+              }}
+            />
+            <Suspense fallback={null}>
               <PixelPageView />
             </Suspense>
           </>
